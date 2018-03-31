2fd5c46ea8cd4a528160f940ce107e038075a0c2
vineet chaudhary <vineetchaudhary131@gmail.com>
2018-04-01 02:27:19 +0530

    DELETE /todos/:id

---
+++ playground/mongoose_remove.js
@@ -0,0 +1,12 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5abff270174c786cbb3262b1').then((todo) => {
+  console.log(todo);
+});
