d8bdb4424247950fcd0928918d72ae880f3372b8
vineet chaudhary <vineetchaudhary131@gmail.com>
2018-03-31 16:45:49 +0530

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 <!-- requiring libraries -->
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -32,6 +33,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    });
+
+
+
+
+
 });
 
 app.listen(3000, () => {
