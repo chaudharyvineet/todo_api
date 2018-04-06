5bc61ee38a9a09b56c15bc00ae9600636bd9fba5
vineet chaudhary <vineetchaudhary131@gmail.com>
2018-04-07 00:05:06 +0530

    private routes and authentication

--- server/models/user.js
+++ server/models/user.js
@@ -7,17 +7,17 @@ var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    minlength: 1,
     trim: true,
+    minlength: 1,
     unique: true,
     validate: {
       validator: validator.isEmail,
-      message: '{VALUE} is not a valid Email'
+      message: '{VALUE} is not a valid email'
     }
   },
   password: {
     type: String,
-    required: true,
+    require: true,
     minlength: 6
   },
   tokens: [{
@@ -29,15 +29,7 @@ var UserSchema = new mongoose.Schema({
       type: String,
       required: true
     }
-  }],
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+  }]
 });
 
 UserSchema.methods.toJSON = function () {
@@ -52,14 +44,35 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens.push({access, token});
+  $push:(
+    {
+      access,
+      token
+    }
+  );
 
   return user.save().then(() => {
     return token;
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = {User}
